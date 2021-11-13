2efaaaf189e2aaf1b235f7775838548750039718
Jatin Gera <gera.jatin@gmail.com>
2021-11-13 16:16:49 +0530

    updated naming convention to journal

--- bareaccount/Journal.py
+++ bareaccount/Journal.py
@@ -7,39 +7,39 @@ class journal:
     def __init__(self, db):
         self.conn = sqlite3.connect(db + '.db')
 
-    def show(self,account):
+    def show(self,journal):
         try:
-            df = pd.read_sql_query("SELECT * FROM {}".format(account), self.conn)
+            df = pd.read_sql_query("SELECT * FROM {}".format(journal), self.conn)
             return df
         except:
-            print("no such table")
+            print("no such journal")
         
-    def getbalance(self,account):
+    def getbalance(self,journal):
         try:
-            cursor = self.conn.execute("SELECT balance FROM {0} WHERE ID = (SELECT MAX(ID)  FROM {0});".format(account))
+            cursor = self.conn.execute("SELECT balance FROM {0} WHERE ID = (SELECT MAX(ID)  FROM {0});".format(journal))
             return cursor.fetchall()[0][0]
         except:
             return 0
     
-    def listAccounts(self):
+    def listJournals(self):
         c = self.conn.cursor()
         c.execute('SELECT name FROM sqlite_master WHERE type="table"')
-        accounts = []
+        journals = []
         for a in c.fetchall():
             #print(a[0])
-            accounts.append(a[0])
+            journals.append(a[0])
             
-        accounts.remove('sqlite_sequence')
-        return accounts
+        journals.remove('sqlite_sequence')
+        return journals
     
-    def deleteAccount(self,name):
+    def deleteJournal(self,name):
         self.conn.execute('DROP TABLE IF EXISTS {};'.format(name))
-        print("Account '{}' deleted.".format(name))
+        print("Journal '{}' deleted.".format(name))
         
     
-    def createAccount(self,name,drop=False):
+    def createJournal(self,name,drop=False):
         if name == "":
-            print("Account name can't be empty")
+            print("Journal name can't be empty")
             return None
         
         tables = self.conn.execute("SELECT name FROM sqlite_master WHERE name='{}';".format(name))
@@ -47,10 +47,10 @@ class journal:
        
         if len(tables.fetchall()) != 0: 
             if drop == True:
-                print("Account '{}' already exists, dropping.".format(name))     
+                print("Journal '{}' already exists, dropping.".format(name))     
                 self.conn.execute('DROP TABLE IF EXISTS {};'.format(name))
             else:
-                print("Account '{}' already exists.".format(name))     
+                print("Journal '{}' already exists.".format(name))     
                 return name       
         
         self.conn.execute('''CREATE TABLE {} (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -58,22 +58,22 @@ class journal:
                                         debit real not null DEFAULT 0.0,
                                         balance real not null ) '''.format(name))
         self.conn.commit()
-        print("Account '{}' Created.".format(name))           
+        print("Journal '{}' Created.".format(name))           
 
         
         return name
            
-    def transaction(self,account,credit,debit):
-        balance = self.getbalance(account)
+    def transaction(self,journal,credit,debit):
+        balance = self.getbalance(journal)
         #print("balance: " + str(balance))
         balance= balance+credit-debit
         #print("after balance: " + str(balance))
         tx = (credit,debit,balance)
-        q = "INSERT INTO {} (credit,debit,balance) VALUES (?, ?, ? );".format(account)
+        q = "INSERT INTO {} (credit,debit,balance) VALUES (?, ?, ? );".format(journal)
         self.conn.execute(q,tx)
         self.conn.commit()
         
-        cursor = self.conn.execute("SELECT balance FROM {0} WHERE ID = (SELECT MAX(ID)  FROM {0});".format(account))
+        cursor = self.conn.execute("SELECT balance FROM {0} WHERE ID = (SELECT MAX(ID)  FROM {0});".format(journal))
         
     def close(self):
         self.conn.close()
--- readme.md
+++ readme.md
@@ -1,2 +1,175 @@
-# Bare Account: Barebone Double Entry Journal liberary for python
+<div class="cell markdown">
 
+# Bare Account: simple double entry journaling liberary for python with SQLite backend
+
+</div>
+
+<div class="cell code" data-execution_count="4">
+
+``` python
+from bareaccount import Journal
+```
+
+</div>
+
+<div class="cell markdown">
+
+### Create an dataset
+
+</div>
+
+<div class="cell code" data-execution_count="5">
+
+``` python
+j = Journal.journal('leadger') 
+```
+
+</div>
+
+<div class="cell markdown">
+
+### Create an empty journal and show it
+
+</div>
+
+<div class="cell code" data-execution_count="16">
+
+``` python
+account = j.createJournal('myjournal',drop=False)
+j.show(account)
+```
+
+<div class="output stream stdout">
+
+    Journal 'myjournal' Created.
+
+</div>
+
+<div class="output execute_result" data-execution_count="16">
+
+    Empty DataFrame
+    Columns: [ID, credit, debit, balance]
+    Index: []
+
+</div>
+
+</div>
+
+<div class="cell markdown">
+
+### Create a transaction and show the journal
+
+</div>
+
+<div class="cell code" data-execution_count="17">
+
+``` python
+j.transaction(account,12,2)
+j.show(account)
+```
+
+<div class="output execute_result" data-execution_count="17">
+
+``` 
+   ID  credit  debit  balance
+0   1    12.0    2.0     10.0
+```
+
+</div>
+
+</div>
+
+<div class="cell markdown">
+
+### get balance of journal
+
+</div>
+
+<div class="cell code" data-execution_count="18">
+
+``` python
+# insert transactions into the journal
+import random
+for i in range(5):
+    debit = random.randint(1,100)   
+    credit = random.randint(1,100)
+    j.transaction(account,credit,0)
+    j.transaction(account,0,debit)
+
+j.show(account)
+```
+
+<div class="output execute_result" data-execution_count="18">
+
+``` 
+    ID  credit  debit  balance
+0    1    12.0    2.0     10.0
+1    2    19.0    0.0     29.0
+2    3     0.0   60.0    -31.0
+3    4    40.0    0.0      9.0
+4    5     0.0   84.0    -75.0
+5    6    82.0    0.0      7.0
+6    7     0.0    4.0      3.0
+7    8    31.0    0.0     34.0
+8    9     0.0    5.0     29.0
+9   10    37.0    0.0     66.0
+10  11     0.0   39.0     27.0
+```
+
+</div>
+
+</div>
+
+<div class="cell code" data-execution_count="19">
+
+``` python
+j.getbalance(account)
+```
+
+<div class="output execute_result" data-execution_count="19">
+
+    27.0
+
+</div>
+
+</div>
+
+<div class="cell markdown">
+
+### list accounting journals
+
+</div>
+
+<div class="cell code" data-execution_count="20">
+
+``` python
+print(j.listJournals())
+```
+
+<div class="output stream stdout">
+
+    ['myjournal']
+
+</div>
+
+</div>
+
+<div class="cell code" data-execution_count="15">
+
+``` python
+[j.deleteJournal(x) for x in j.listJournals()]
+```
+
+<div class="output stream stdout">
+
+    Journal 'myjournal' deleted.
+
+</div>
+
+<div class="output execute_result" data-execution_count="15">
+
+    [None]
+
+</div>
+
+</div>
